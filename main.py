51e5811b210e81a30d4ed97d64b55afe30ddc1d7
abulyousuf <abulyousuf@outlook.com>
2025-09-08 17:39:53 +0100

    count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def get_book_text(path_to_file):
   with open(path_to_file) as f:
@@ -11,6 +11,9 @@ def main():
   
   num_words = get_num_words(book_contents)
 
+  num_chars = get_num_chars(book_contents)
+
   print(f"{num_words} words found in the document")
+  print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,20 @@ def get_num_words(book_text):
   number_of_words = len(book_text.split())
   
   return number_of_words
+
+def get_num_chars(book_text):
+  freq = {}
+
+  for char in book_text.lower():
+    if char == " ":
+      if char in freq:
+        freq[char] += 1
+      else:
+        freq[char] = 1
+
+    if char in freq:
+      freq[char] += 1
+    else:
+      freq[char] = 1
+      
+  return freq
