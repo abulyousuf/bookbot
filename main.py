43a0e2eecf5220af443faa8dcdb14455079916a4
abulyousuf <abulyousuf@outlook.com>
2025-09-08 22:03:09 +0100

    use sys module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_num_words, get_num_chars, get_sorted_list
 
 def get_book_text(path_to_file):
@@ -7,7 +9,11 @@ def get_book_text(path_to_file):
   return file_contents
 
 def main():
-  book_contents = get_book_text("./books/frankenstein.txt")
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  book_contents = get_book_text(sys.argv[1])
   
   num_words = get_num_words(book_contents)
 
@@ -16,7 +22,7 @@ def main():
   sorted_list = get_sorted_list(num_chars)
 
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {sys.argv[1]}...")
   print("----------- Word Count -----------")
   print(f"Found {num_words} total words")
   print("--------- Character Count ---------")
