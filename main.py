91ff916fab2dfe865173c7d0b582ee5ae057ad22
abulyousuf <abulyousuf@outlook.com>
2025-09-08 21:14:09 +0100

    print a report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, get_sorted_list
 
 def get_book_text(path_to_file):
   with open(path_to_file) as f:
@@ -13,7 +13,14 @@ def main():
 
   num_chars = get_num_chars(book_contents)
 
-  print(f"{num_words} words found in the document")
-  print(num_chars)
+  sorted_list = get_sorted_list(num_chars)
+
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count -----------")
+  print(f"Found {num_words} total words")
+  print("--------- Character Count ---------")
+  for i in sorted_list:
+    print(f"{i["char"]}: {i["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -8,14 +8,26 @@ def get_num_chars(book_text):
 
   for char in book_text.lower():
     if char == " ":
-      if char in freq:
-        freq[char] += 1
-      else:
-        freq[char] = 1
+      continue
+
+    if char == "\n":
+      continue
 
     if char in freq:
       freq[char] += 1
     else:
       freq[char] = 1
-      
+
   return freq
+
+def sort_on(items):
+  return items["num"]
+
+def get_sorted_list(num_chars_dict):
+  key_value_dict = [
+    {"char": c, "num": n}
+    for c, n in num_chars_dict.items()
+  ]
+
+  key_value_dict.sort(reverse=True, key=sort_on)
+  return key_value_dict
